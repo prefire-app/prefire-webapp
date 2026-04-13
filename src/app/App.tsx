216bc92af1519cf6b5c0fb51a39d823ad2ea882b
Jack Lesemann <jlesemann@captechconsulting.com>
2026-04-13 17:37:23 -0600

    added s3 deployment and deploy script

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,34 +4,36 @@ import NavBar from "../components/NavBar";
 import HomeHero from "../components/HomeHero";
 import Learning from "../components/Learning";
 import MappingTool from "../components/MappingTool";
+import Donate from "../components/Donate";
 
 function App() {
-  return (
-    <Router>
-      <div className="min-h-screen bg-[#753742]">
-        <header className="flex items-center justify-between p-4">
-          <div className="flex w-full items-center">
-            <div className="flex-1 flex justify-start">
-              <Logo />
+    return (
+        <Router>
+            <div className="min-h-screen bg-[#753742]">
+                <header className="flex items-center justify-between p-4">
+                    <div className="flex w-full items-center">
+                        <div className="flex-1 flex justify-start">
+                            <Logo />
+                        </div>
+                        <div className="flex-1 flex justify-center">
+                            <NavBar />
+                        </div>
+                        <div className="flex-1 flex justify-end">
+                            {/* Right column, leave empty or add future content */}
+                        </div>
+                    </div>
+                </header>
+                <main>
+                    <Routes>
+                        <Route path="/" element={<HomeHero />} />
+                        <Route path="/map" element={<MappingTool />} />
+                        <Route path="/learning" element={<Learning />} />
+                        <Route path="/donate" element={<Donate />} />
+                    </Routes>
+                </main>
             </div>
-            <div className="flex-1 flex justify-center">
-              <NavBar />
-            </div>
-            <div className="flex-1 flex justify-end">
-              {/* Right column, leave empty or add future content */}
-            </div>
-          </div>
-        </header>
-        <main>
-          <Routes>
-            <Route path="/" element={<HomeHero />} />
-            <Route path="/map" element={<MappingTool />} />
-            <Route path="/learning" element={<Learning />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
-  );
+        </Router>
+    );
 }
 
 export default App;
